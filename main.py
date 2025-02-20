2c9cc070e7d108c41fc68cd2f93ec44b436b4ca1
DomiStjls <daryashev4enko@yandex.ru>
2025-02-20 17:30:14 +0300

    web part is ready

---
+++ main.py
@@ -0,0 +1,28 @@
+from flask import Flask, request, render_template
+
+
+app = Flask(__name__)
+
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/upload", methods=["POST"])
+def upload_file():
+    if "file" not in request.files:
+        return "No file part"
+
+    file = request.files["file"]
+
+    if file.filename == "":
+        return "No selected file"
+
+    if file:
+        return "File uploaded successfully"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
